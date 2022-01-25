557c8727b5e69385d57f70bd55e5a3fbdfa4e6d5
Andreas <ciscoheat@gmail.com>
2022-01-25 21:07:06 +0100

    Storage update, game fixes, keyboard shortcuts.

--- src/lib/appStorage.ts
+++ src/lib/appStorage.ts
@@ -1,28 +1,32 @@
 import { LocalStorage } from "./storage"
 import { initialState as gameState } from "../game"
 
-export const newState = () => ({
+export interface AppState {
+    readonly rounds: typeof gameState.rounds,
+    readonly rules: typeof gameState.rules,
+    readonly players: Readonly<FixedLengthArray<2, string>>
+}
+
+const appState = () => ({
     rounds: gameState.rounds,
     rules: gameState.rules,
-    players: ['', '']
-})
+    players: ['', ''] as const
+}) as const
 
-const state = newState()
-
-export class AppStorage extends LocalStorage<typeof state> {
+export class AppStorage extends LocalStorage<AppState> {
     constructor() {
-        super(newState)
-        this.throwOnIntegrityError = false
-        this.throwOnUpgradeError = false
+        super(appState)
+        this.configuration.throwOnIntegrityError = false
+        this.configuration.throwOnUpgradeError = false
     }
 
-    protected _load(key: string): typeof state {
+    protected _load(key: string): AppState {
         const v = super._load(key)
         //console.log('Loaded', key, v)
         return v
     }
 
-    protected _save(key: string, value: typeof state): void {
+    protected _save(key: string, value: AppState): void {
         super._save(key, value)
         //console.log('Saved', key, value)
     }
